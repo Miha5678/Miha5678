bf43cad60ae3c498b4190171dfec611b7d0d005a
Miha5678 <150503733+Miha5678@users.noreply.github.com>
2024-03-22 14:52:21 +1000

    Add files via upload

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const port = process.env.PORT || 3000
 
 
 app.listen (3000, function() {
-    console.log ('Server has been started')
+    console.log ('Server has been started on ${port} port')
 })
